4d35e30ef88edc8dfed1d7f525558d797b0e7ff4
pierrechass <pierrechass@gmail.com>
2023-10-06 12:53:50 +0200

    Added License

---
+++ q-cirl/policies.py
@@ -0,0 +1,20 @@
+# policies.py : Policies for the Q-CIRL algorithm. Worked based on [1].
+#
+# Copyright (C) 2023  Pierre Chassagne
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see https://github.com/pierrechass/SB3_CIRL/main/LICENSE.
+#
+# [1] A. Schlaginhaufen and M. Kamgarpour. Identifiability and generalizability in
+#     constrained inverse reinforcement learning. 2023.
+#     doi: 10.48550/ARXIV.2306.00629. URL https://arxiv.org/abs/2306.00629.
